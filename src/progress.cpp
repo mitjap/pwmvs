240e354ce9d87fc7b7dabd53154b8abad046a8d2
mitjap <mitjap@gmail.com>
2018-09-05 11:56:14 +0200

    flush progress on increment

--- src/progress.cpp
+++ src/progress.cpp
@@ -68,7 +68,7 @@ void ConsoleProgress::increment(int steps)
 {
     AbstractProgress::increment(steps);
 
-    std::cout << "Progress: " << completedSteps() << "/" << totalSteps() << "\n";
+    std::cout << "Progress: " << completedSteps() << "/" << totalSteps() << "\n" << std::endl;
 }
 
 void ConsoleProgress::configure(int total_steps)
